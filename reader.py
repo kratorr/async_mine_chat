49fc9b76edcc90c65338da608382eac3a3d795f2
Vladimir Smirnov <smirnovv91@gmail.com>
2021-04-19 22:52:08 +0300

    Add CLI options.

--- reader.py
+++ reader.py
@@ -1,11 +1,50 @@
 import asyncio
+import aiofiles
+import argparse
+import os
+
+from datetime import date, datetime
 
 
-async def read_chat():
-    reader, _ = await asyncio.open_connection('minechat.dvmn.org', 5000)
-    while True:
-        data = await reader.readline()
-        print(data.decode().replace('\n', ''))
+
+parser = argparse.ArgumentParser(description='Async chat client for minechat.')
 
 
-asyncio.run(read_chat())
+parser.add_argument('-H',
+                    '--host',
+                    type=str,
+                    default=os.getenv('HOST', default='minechat.dvmn.org'),
+                    help='Host address: default=minechat.dvmn.org',
+                    )
+
+parser.add_argument('-p',
+                    '--port',
+                    type=int,
+                    default=os.getenv('PORT', default=5000),
+                    help='Port; default=5000',
+                    )
+
+parser.add_argument('--history',
+                    type=str,
+                    default=os.getenv('LOG', default='./chat.log'),
+                    help='Chat log file path; default=./chat.log',
+                    )
+
+
+async def read_chat(host, port, history_path):
+    reader, _ = await asyncio.open_connection(host, port)
+    async with aiofiles.open(history_path, mode='a') as file:
+        
+        while True:
+            data = await reader.readline()
+            message = data.decode().replace('\n', '')
+            now = datetime.now().strftime("%d.%m.%y %H:%m")
+            print(message)
+            await file.writelines(f"[{now}] {message}\n")
+            await file.flush()
+            
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    host, port, history_path = args.host, args.port, args.history
+    asyncio.run(read_chat(host, port, history_path))
