f4ad523b4df56282a4d3803d03753c9f2b895888
Vladimir Smirnov <smirnovv91@gmail.com>
2021-04-19 23:06:10 +0300

    Setup logging.

--- client.py
+++ client.py
@@ -2,8 +2,16 @@ import asyncio
 import aiofiles
 import argparse
 import os
+import logging
 
 from datetime import date, datetime
+
+from asyncio.tasks import sleep
+
+
+logger = logging.getLogger(__file__)
+
+logging.basicConfig(filename='client.log', level=logging.DEBUG)
 
 
 
@@ -21,20 +29,23 @@ parser.add_argument('-p',
                     '--port',
                     type=int,
                     default=os.getenv('PORT', default=5050),
-                    help='Port; default=5000',
+                    help='Port; default=5050',
                     )
 
 
+async def send_data(writer, data):
+    logger.debug(data)
+    writer.write(data)
+    
 async def chat_client(host, port):
     reader, writer = await asyncio.open_connection(host, port)
         
     while True:
         data = await reader.readline()
         message = data.decode().replace('\n', '')
-        now = datetime.now().strftime("%d.%m.%y %H:%m")
-        print(message)
-        writer.write('4206afde-9c2c-11eb-8c47-0242ac110002\n'.encode('utf-8'))
-        writer.write('test message\n\n'.encode('utf-8'))
+        logger.debug(message)
+        await send_data(writer, '4206afde-9c2c-11eb-8c47-0242ac110002\n'.encode('utf-8'))
+        await send_data(writer, 'test message\n\n'.encode('utf-8'))
         break
     writer.close()
         
