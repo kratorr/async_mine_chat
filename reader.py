65c01a6288d027a3a513cc48b174e1a92ce9ee08
Vladimir Smirnov <smirnovv91@gmail.com>
2022-06-13 15:37:58 +0300

    Add excepions for connect to chat

--- reader.py
+++ reader.py
@@ -2,6 +2,7 @@ import asyncio
 import aiofiles
 import argparse
 import os
+import time
 
 from datetime import  datetime
 
@@ -32,17 +33,30 @@ parser.add_argument('--history',
 
 
 async def read_chat(host, port, history_path):
-    reader, _ = await asyncio.open_connection(host, port)
-    async with aiofiles.open(history_path, mode='a') as file:
-        
-        while True:
-            data = await reader.readline()
-            message = data.decode().replace('\n', '')
-            now = datetime.now().strftime("%d.%m.%y %H:%m")
-            print(message)
-            await file.writelines(f"[{now}] {message}\n")
-            await file.flush()
-   
+    tries = 10
+
+    for number_of_tries in range(1, tries):
+        try:
+            if number_of_tries > 3:
+               time.sleep(5)
+            reader, writer = await asyncio.open_connection(host, port)
+            break
+        except Exception as e:
+            print(e)
+            print("Connection error, attempt to reconnect: ", number_of_tries)
+
+    try:
+        async with aiofiles.open(history_path, mode='a') as file:
+
+            while True:
+                data = await reader.readline()
+                message = data.decode().replace('\n', '')
+                now = datetime.now().strftime("%d.%m.%y %H:%m")
+                print(message)
+                await file.writelines(f"[{now}] {message}\n")
+                await file.flush()
+    finally:
+        writer.close()
 
 if __name__ == '__main__':
     args = parser.parse_args()
